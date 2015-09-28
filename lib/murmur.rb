8011731e21605b2f97c0b1cb3ce22a939c1d843e
Ranndom <ranndom@rnndm.xyz>
2015-09-29 00:03:07 +1000

    Added Murmur.client interface.

--- lib/murmur.rb
+++ lib/murmur.rb
@@ -1,6 +1,20 @@
 require 'murmur/version'
+require 'murmur/ice_interface'
 require 'murmur/server'
 
 module Murmur
-    
+
+    def self.client(options={})
+        @client = Murmur::Meta.new(options)
+    end
+
+    def self.method_missing(method, *args, &block)
+        return super unless @client.respond_to?(method)
+        @client.send(method, *args, &block)
+    end
+
+    def self.respond_to?(method)
+        return @client.respond_to?(method) || super
+    end
+
 end
